88e86af25cdf27b12c1753762db4181b6684f9f0
Silto <silte00@gmail.com>
2016-04-19 00:39:42 +0200

    Async loading of files (output, api key, channels list), strict mode enabled, ES6 synthax adopted (arrow func, promises, block-scoped declaration)

--- main.js
+++ main.js
@@ -1,13 +1,37 @@
+"use strict";
+
 var google = require ('googleapis');
 var fs = require('fs');
 //var readline = require('readline');
 var readlineSync = require('readline-sync');
 
+const readline = require('readline');
+
+
+
 var depthpipe = 4;
 var outpipe = "logstats.txt";
 var titleLine = "\nchannelId , channelName , views , likes , ratio , duration , comments , age";
+
+
 var re_depth = /--depth=(\d+)/i;
 var re_out = /--output=([A-Za-z0-9_\.\-]+)/i;
+
+var youtube_api_key = null;
+var youtube = null;
+
+var channel_ids = ['UCXIYLgIp6DYZHjmUUUXErmg'];
+
+//variables de temporisation
+const MAX_DAILY = 50000000;
+const MAX_100S = 300000;
+const MAX_20S  = Math.min(MAX_100S/100, MAX_DAILY/(24*3600))*20;
+var quota_20s = 0;
+var quota_total = 0;
+var start_time = 0;
+var last_20s_check = 0;
+
+//read arguments
 process.argv.forEach(function(val) {
     var depthParam = val.match(re_depth);
     if(depthParam !== null){
@@ -31,69 +55,124 @@ process.argv.forEach(function(val) {
     }
 });
 
-try {
-    var stats = fs.statSync(outpipe);
-    console.log("output file exists");
-    // Wait for user's response.
-    var answer = readlineSync.question('Do you want to overwrite(o)?append(a)? ');
-    if (answer == "o") {
-        fs.writeFileSync(outpipe,titleLine,"utf8");
-    }
-} catch (err) {
-    if(err.code == 'ENOENT') {
-        console.log("output file doesn't exists, will be created");
-        try {
-            fs.writeFileSync(outpipe,titleLine,"utf8");
-        }catch(e){
-            console.log("could not write title line to file.");
-        }
-    } else {
-        console.log("output file doesn't exists or something else happened : ", err.code);
-        try {
-            fs.writeFileSync(outpipe,titleLine,"utf8");
-        }catch(e){
-            console.log("could not write title line to file.");
-        }
-    }
-}
-var api_key_file;
-try {
-    api_key_file = fs.readFileSync('yt_api_key.txt','utf8');
-} catch (e) {
-    if (e.code === 'ENOENT') {
-        console.error('File not found!');
-    } else {
-        throw e;
-    }
-}
 
-var key_regexp = /[A-Za-z0-9\-]{30,}/;
-var api_key = key_regexp.exec(api_key_file);
-if (api_key === null) {
-    console.error("google API key not found");
-    process.exit(1);
-}else{
-    api_key = api_key[0];
-    console.log("google API key : "+api_key);
-}
+var openFiles = function(){
 
-google.options ({ auth: api_key });
-var youtube = google.youtube ('v3');
+    var criticalError = function(err){
+        console.error("ERR : " + err);
+        process.exit(1);
+    };
 
-var channel_ids = fs.readFileSync('yt_ids.txt').toString().split("\n");
-while (channel_ids[channel_ids.length - 1] === "" || channel_ids[channel_ids.length - 1] == " ") {
-    channel_ids.pop();
-}
-console.log(channel_ids.length+" channels loaded");
+    var outOpen = new Promise((resolve, reject) => {
+        fs.stat(outpipe,(err, stats) => {
+            if(err){
+                if(err.code == 'ENOENT') {
+                    console.log("output file doesn't exists, will be created");
+                } else {
+                    console.log("output file doesn't exists or something else happened : ", err.code);
+                }
+                fs.writeFile(outpipe,titleLine,"utf8", (err) => {
+                    if (err) {
+                        reject("could not write title line to file.");
+                    }else{
+                        resolve();
+                    }
+                });
+            }else{
+                console.log("output file exists");
 
-//variables de temporisation
-var MAX_DAILY = 50000000;
-var MAX_100S = 300000;
-var MAX_20S  = Math.min(MAX_100S/100, MAX_DAILY/(24*3600))*20;
-var quota_20s = 0;
-var quota_total = 0;
-var start_time = 0;
-var last_20s_check = 0;
+                let rl = readline.createInterface({
+                    input: process.stdin,
+                    output: process.stdout
+                });
+
+                rl.question('Do you want to overwrite(o)?append(a)? ', (answer) => {
+                    if (answer == "o") {
+                        fs.writeFile(outpipe,titleLine,"utf8", (err) => {
+                            if (err) {
+                                reject("could not write title line to file.");
+                            }else{
+                                resolve();
+                            }
+                        });
+                    }else{
+                        resolve();
+                    }
+                rl.close();
+                });
+            }
+        });
+    })
+    .then(() => {
+        console.log("Success opening output file.");
+        var apiKeyOpen = new Promise((resolve, reject) => {
+
+            fs.readFile('yt_api_key.txt','utf8', (err,api_key_file) => {
+                if(err){
+                    if (err.code === 'ENOENT') {
+                        reject('Key file not found!');
+                    }else{
+                        reject("unknown error opening api key file.");
+                    }
+
+                }else{
+                    let key_regexp = /[A-Za-z0-9\-]{30,}/;
+                    let api_key = key_regexp.exec(api_key_file);
+                    if (api_key === null) {
+                        reject("google API key not found in provided file");
+                    }else{
+                        resolve(api_key[0]);
+                        
+                    }
+                }
+            });
+        })
+        .then((api_key) => {
+            youtube_api_key = api_key;
+            console.log("Success opening API key file.");
+
+            var channelListOpen = new Promise((resolve, reject) => {
+                fs.readFile('yt_ids.txt',"utf8",(err, data) => {
+                    if (err) {
+                        reject(err);
+                    }else{
+                        let channels = data.split("\n");
+                        while (channels[channels.length - 1] === "" || channels[channels.length - 1] == " ") {
+                            channels.pop();
+                        }
+                        console.log(channels.length+" channels loaded");
+                        channel_ids = channels;
+                        resolve();
+                    }
+                });
+
+            })
+            .then(API_init)
+            .catch((err) => { 
+                console.error("non-critical error reading channel list :\n"+err);
+                API_init();
+            });
+        })
+        .catch(criticalError);
+
+    })
+    .catch(criticalError);
+
+};
+
+var API_init = function(){
+    google.options ({ auth: youtube_api_key });
+    youtube = google.youtube ('v3');
+    console.log("API initialised");
+
+    //start searching
+    start_time = Date.now();
+    chainSearch({depth: depthpipe, channelIds: channel_ids, postAction:like_view_analyzer});
+
+};
+
+
+
 var quota_update = function(increm){
     quota_20s+=increm;
     quota_total+=increm;
@@ -134,117 +213,83 @@ function search_youtube (query, callback) {
     if (query.q !== undefined) {
         queryObject.q = query.q;
     }
-  if (query.pageToken !== undefined) {
+    if (query.pageToken !== undefined) {
       queryObject.pageToken = query.pageToken;
-  }
-  if (query.maxResults !==undefined) {
+    }
+    if (query.maxResults !==undefined) {
       queryObject.maxResults = query.maxResults;
-  }if (query.channel !== undefined) {
+    }if (query.channel !== undefined) {
       queryObject.channelId = query.channel;
-  }
-  var videos = [];
-  var detailsLoop = function(res, index, errors, outfunction){
-      //console.log(res);
-      if (index >= res.length) {
-          if (errors === '') {
-              outfunction(null, videos);
-          }else{
-              outfunction(errors, videos);
-          }
-
-      }else{
-          var video = {
-            id: res[index].id.videoId,
-            title: res[index].snippet.title || '',
-            channelTitle: res[index].snippet.channelTitle,
-            channelId: res[index].snippet.channelId,
-            timeSincePublished: Math.floor((Date.now() - Date.parse(res[index].snippet.publishedAt))/1000)
-          };
-          console.log("Qt: "+quota_total+" fetching details for video "+ index + "("+video.title+")");
-
-          if (quota_check() === false) {
-              setTimeout(function(){console.log("temporisation");detailsLoop(res,index,errors,outfunction);},1000);
-          }else{
-              quota_update(1+2+2);
-              youtube.videos.list (
+    }
+    var videos = [];
+    var detailsLoop = function(res, index, errors, outfunction){
+        //console.log(res);
+        if (index >= res.length) {
+            if (errors === '') {
+                outfunction(null, videos);
+            }else{
+                outfunction(errors, videos);
+            }
+  
+        }else{
+            var video = {
+                id: res[index].id.videoId,
+                title: res[index].snippet.title || '',
+                channelTitle: res[index].snippet.channelTitle,
+                channelId: res[index].snippet.channelId,
+                timeSincePublished: Math.floor((Date.now() - Date.parse(res[index].snippet.publishedAt))/1000)
+            };
+            console.log("Qt: "+quota_total+" fetching details for video "+ index + "("+video.title+")");
+  
+            if (quota_check() === false) {
+                setTimeout(function(){console.log("temporisation");detailsLoop(res,index,errors,outfunction);},1000);
+            }else{
+                quota_update(1+2+2);
+                youtube.videos.list (
                 {
-                  part: 'statistics, contentDetails',
-                  id: video.id
+                    part: 'statistics, contentDetails',
+                    id: video.id
                 },
                 function (err2, data) {
-                  if (err2) { detailsLoop(res, index+1, errors+"\n"+err2, outfunction); }
-                  if (data.items.length >= 1) {
-                    data.items[0].contentDetails.duration.replace (/PT(\d+)M(\d+)S/, function (t, m, s) {
-                      video.duration = (parseInt (m) *60) + parseInt (s);
-                    });
-                    video.definition = data.items[0].contentDetails.definition;
-                      video.views = parseInt(data.items[0].statistics.viewCount);
-                      video.likes = parseInt(data.items[0].statistics.likeCount);
-                      video.dislikes = parseInt(data.items[0].statistics.dislikeCount);
-                      video.comments = parseInt(data.items[0].statistics.commentCount);
-                    videos.push(video);
-
-                    detailsLoop(res, index+1, errors, outfunction);
+                    if (err2) { detailsLoop(res, index+1, errors+"\n"+err2, outfunction); }
+                    if (data.items.length >= 1) {
+                        data.items[0].contentDetails.duration.replace (/PT(\d+)M(\d+)S/, 
+                            function (t, m, s) {
+                                video.duration = (parseInt (m) *60) + parseInt (s);
+                        });
+                        video.definition = data.items[0].contentDetails.definition;
+                        video.views = parseInt(data.items[0].statistics.viewCount);
+                        video.likes = parseInt(data.items[0].statistics.likeCount);
+                        video.dislikes = parseInt(data.items[0].statistics.dislikeCount);
+                        video.comments = parseInt(data.items[0].statistics.commentCount);
+                        videos.push(video);
+  
+                        detailsLoop(res, index+1, errors, outfunction);
+                    }
                   }
-                }
-              );
-          }
-
-      }
-
-  };
-  if (quota_check() === false) {
-      setTimeout(function(){console.log("temporisation");search_youtube(query, callback);},1000);
-  }else{
-      quota_update(100);
-      youtube.search.list (
-        queryObject,
-        function (err, res) {
-          if (err) { return callback (err); }
-
-
-          detailsLoop(res.items, 0,'', callback);
-
-        //   res.items.forEach (function (result) {
-        //     var video = {
-        //       id: result.id.videoId,
-        //       //urlShort: 'http://youtu.be/'+ result.id.videoId,
-        //       //urlLong: 'http://www.youtube.com/watch?v='+ result.id.videoId,
-        //       //published: result.snippet.publishedAt,
-        //       title: result.snippet.title || '',
-        //       //description: result.snippet.description || '',
-        //       //images: result.snippet.thumbnails,
-        //       channelTitle: result.snippet.channelTitle,
-        //       //channelId: result.snippet.channelId,
-        //       //live: result.snippet.liveBroadcastContent || ''
-        //     };
-          //
-        //     youtube.videos.list (
-        //       {
-        //         part: 'statistics, contentDetails',
-        //         id: video.id
-        //       },
-        //       function (err2, data) {
-        //         if (err2) { return callback (err2); }
-        //         if (data.items.length >= 1) {
-        //           data.items[0].contentDetails.duration.replace (/PT(\d+)M(\d+)S/, function (t, m, s) {
-        //             video.duration = (parseInt (m) *60) + parseInt (s);
-        //           });
-        //           video.definition = data.items[0].contentDetails.definition;
-        //             video.views = data.items[0].statistics.viewCount;
-        //             video.likes = data.items[0].statistics.likeCount;
-        //             video.dislikes = data.items[0].statistics.dislikeCount;
-        //             video.comments = data.items[0].statistics.commentCount;
-        //           videos.push(video);
-        //         }
-        //       }
-        //     );
-        //   });
-        //   callback(videos.length)
-
+                );
+            }
+  
         }
-      );
-  }
+  
+    };
+    if (quota_check() === false) {
+        setTimeout(function(){
+            console.log("temporisation");
+            search_youtube(query, callback);
+        },1000);
+    }else{
+        quota_update(100);
+        youtube.search.list (
+            queryObject,
+            function (err, res) {
+                if (err) { return callback (err); }
+  
+                detailsLoop(res.items, 0,'', callback);
+  
+            }
+        );
+    }
 }
 
 var like_view_analyzer = function(err, videos, callback){
@@ -302,7 +347,7 @@ var like_view_analyzer = function(err, videos, callback){
 };
 
 //search sequence
-start_time = Date.now();
+// start_time = Date.now();
 //search_youtube({channel:'UCXIYLgIp6DYZHjmUUUXErmg', maxResults: 50},like_view_analyzer);
 
 var defaultPost = function(err, videos, callback){
@@ -347,4 +392,4 @@ var chainSearch = function(params){
     scheduler(-1);
 };
 
-chainSearch({depth: depthpipe, channelIds: channel_ids, postAction:like_view_analyzer});
+openFiles();
